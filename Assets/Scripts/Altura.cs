f1b3a7b4c15f55476e650912e3f10149e870316e
Guilherme Galarça Dutra <udutra@gmail.com>
2017-10-17 18:27:01 -0200

    Aula 17
    ok

---
+++ Assets/Scripts/Altura.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Altura : MonoBehaviour {
+
+	public Transform personagem;
+	Text textInterface;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Debug.Log ("Altura: " + personagem.position.y);
+	}
+}
